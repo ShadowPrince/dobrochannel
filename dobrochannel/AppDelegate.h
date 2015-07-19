bf351f05dc18d0e778d3d04c0105f5423d6f906f
Vasiliy Horbachenko <shadow.prince@ya.ru>
2015-07-19 16:08:57 +0300

    Initial Commit

---
+++ dobrochannel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dobrochannel
+//
+//  Created by shdwprince on 7/19/15.
+//  Copyright © 2015 Vasiliy Horbachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dobrochannel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dobrochannel
+//
+//  Created by shdwprince on 7/19/15.
+//  Copyright © 2015 Vasiliy Horbachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
