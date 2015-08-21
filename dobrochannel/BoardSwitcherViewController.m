a192e37685940197194bb0521da62c1c3906719c
Vasiliy Horbachenko <shadow.prince@ya.ru>
2015-08-21 16:49:34 +0300

    cherry pick ec68af2: fixes

--- dobrochannel/BoardSwitcherViewController.m
+++ dobrochannel/BoardSwitcherViewController.m
@@ -59,7 +59,9 @@
     NSArray<NSString *> *board = self.boardsData[key];
 
     UIButton *b = (UIButton *) [cell viewWithTag:100];
-    [b setTitle:[NSString stringWithFormat:@"/%@/", key] forState:UIControlStateNormal];
+    [UIView performWithoutAnimation:^{
+        [b setTitle:[NSString stringWithFormat:@"/%@/", key] forState:UIControlStateNormal];
+    }];
 
     UILabel *l = (UILabel *) [cell viewWithTag:102];
     l.text = board[0];
